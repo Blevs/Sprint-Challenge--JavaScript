39a618b2e037ed142d6ed3f475a1e4c22ff92b20
Henry Blevins <heblevi@gmail.com>
2019-03-01 11:27:28 -0500

    Completed functions

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(a, b, cb) {
+    return cb(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,18 +17,46 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function greeting(first, last) {
+    return `Hello ${first} ${last}, nice to meet you!`
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
-// Explain in your own words why `nestedfunction()` can access the variable `internal`.
+// Explain in your own words why `nestedFunction()` can access the variable `internal`.
 
 // Explanation: 
+// `nestedFunction` is able to access the variable `internal` because of
+// scoped variable resolution and closures. When JavaScript encounters the
+// reference to the variable `internal` it first tries to resolve it within the
+// local scope (inside the `nestedFunction`), and does not find it. It then
+// proceeds to the outer scope (inside `myFunction`) and finds the variable
+// definition accesses it's value.
+// `nestedFunction` would even be able to access the variable `external` as the
+// resolution would proceed all the way to the global scope while trying to
+// resolve a variable.
+// Closures come into play because `nestedFunction` would still be able to
+// access the `internal` variable even if the function was executed outside of
+// the scope in which `internal` was declared. This is because JavaScript uses
+// lexical, and not dynamic, scoping. The environment in which the function was
+// declared, its lexical scope, is captured and when `nestedFunction` is
+// executed, even in a very different context, resolution will still see the
+// definition scope, even if we don't, and the values within this scope are //
+// still able to be resolved, referenced and modified.
 
 
 const external = "I'm outside the function";
